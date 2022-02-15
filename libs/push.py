a8e79dbdbdcf1d4d2cc376fef7b4b0911212e204
gongkangjia <gongkangjia@gmail.com>
2022-02-15 11:01:03 +0800

    新增推送邮箱

--- libs/push.py
+++ libs/push.py
@@ -102,8 +102,8 @@ class EmailPush(Push):
         logger.info("开始发送邮件 =>{},{},{}", subject, contents, attachments)
 
         sta = self.yag.send(to=[
-            # "njhbjwkc@163.com",
-            # "njhbjdqcgy@163.com"
+            "qq45934861@163.com",
+            "njhbjdqcgy@163.com"
             # "kjgong@kjong.cn"
         ],
             cc="kjgong@kjgong.cn",
